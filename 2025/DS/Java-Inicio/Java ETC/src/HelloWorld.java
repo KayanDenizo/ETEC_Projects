42d1c58afc391e8b635c0eec2b9e81725c12c731
KAYAN YUKUHIRO DE NIZO <KAYAN_4644@uirapuru.br>
2025-02-12 11:27:48 -0300

    Adicionando arquivo javateste

---
+++ 2025/DS/Java-Inicio/Java ETC/src/HelloWorld.java
@@ -0,0 +1,12 @@
+
+
+public class HelloWorld {
+    public static void main(String[] args)
+    {
+        StringBuilder sb = new StringBuilder(10);
+
+        sb.append("QuickRed");
+
+        sb.delete(5, 9);
+    }
+}
