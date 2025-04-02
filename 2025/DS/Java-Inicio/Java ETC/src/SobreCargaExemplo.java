c1adb344a88c791df79d919ff1d62e276de8fbb2
KAYAN YUKUHIRO DE NIZO <KAYAN_4644@uirapuru.br>
2025-04-02 12:46:09 -0300

    mais commit

--- 2025/DS/Java-Inicio/Java ETC/src/SobreCargaExemplo.java
+++ 2025/DS/Java-Inicio/Java ETC/src/SobreCargaExemplo.java
@@ -1,8 +1,6 @@
 public class SobreCargaExemplo {
     public void exibir(int x) {
         System.out.println("Valor inteiro " + x);
-
-
     }
 
     public void exibir(String texto) {
@@ -11,5 +9,12 @@ public class SobreCargaExemplo {
 
     public void exibir(int x, String texto) {
         System.out.println(x + " - " + texto);
+    }
+
+    public static void main(String[] args) {
+        SobreCargaExemplo sobrecarga = new SobreCargaExemplo();
+        sobrecarga.exibir(10);
+        sobrecarga.exibir("Exemplo");
+        sobrecarga.exibir(7, "Java");
     }
 }
